0ca89d34946113e6d2b8b747ed6240820a8be039
jordan byrd <jabyrd3@gmail.com>
2016-07-30 22:07:56 -0400

    heh starting to feel homier

---
+++ api/app.js
@@ -0,0 +1,113 @@
+'use strict';
+var express = require('express');
+var path = require('path');
+// var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+
+// postgres config, connection pool setup
+
+// create a config to configure both pooling behavior
+// and client options
+// note: all config is optional and the environment variables
+// will be read if the config is not present
+var config = {
+    // user: '{{dbuser}}', //env var: PGUSER
+    // database: '{{dbname}}', //env var: PGDATABASE
+    // password: '{{dbpass}}', //env var: PGPASSWORD
+    user: 'admin.jbyrd', //env var: PGUSER
+    database: 'pokemong', //env var: PGDATABASE
+    password: '', //env var: PGPASSWORD
+    port: 5432, //env var: PGPORT
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+};
+
+
+//this initializes a connection pool
+//it will keep idle connections open for a 30 seconds
+//and set a limit of maximum 10 idle clients
+var pool = new pg.Pool(config);
+
+// to run a query we can acquire a client from the pool,
+// run a query on the client, and then return the client to the pool
+pool.connect(function(err, client, done) {
+    if (err) {
+        return console.error('error fetching client from pool', err);
+    }
+    // sample query; need to pass pool into app somehow.
+    client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+        //call `done()` to release the client back to the pool
+        done();
+
+        if (err) {
+            return console.error('error running query', err);
+        }
+        console.log(result.rows[0].number);
+        //output: 1
+    });
+});
+
+pool.on('error', function(err, client) {
+    // if an error is encountered by a client while it sits idle in the pool
+    // the pool itself will emit an error event with both the error and
+    // the client which emitted the original error
+    // this is a rare occurrence but can happen if there is a network partition
+    // between your application and the database, the database restarts, etc.
+    // and so you might want to handle it and at least log it out
+    console.error('idle client error', err.message, err.stack);
+});
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// make sure this is what you want before going live!
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({ error: err });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({ error: err });
+});
+
+
+module.exports = app;
